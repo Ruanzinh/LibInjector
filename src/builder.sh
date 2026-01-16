857a4cdf8b7976918c248db39c605b4d588df27a
Ruanzinh <96693351+Ruanzinh@users.noreply.github.com>
2026-01-16 11:29:15 -0300

    Builder updated with my changes.

--- src/builder.sh
+++ src/builder.sh
@@ -13,9 +13,9 @@ echo "[*] Generating Base64 (it may take some seconds)..."
 B64_DATA=$(base64 "$INJECTOR_BIN" | tr -d '\n')
 
 echo "[*] Creating final script..."
-cat <<EOF > "$OUTPUT_SCRIPT"
-#!/system/bin/sh
-# INJETOR AUTOMATICO - GERADO VIA BUILDER
+cat <<'EOF' > "$OUTPUT_SCRIPT"
+# TELEGRAM: "@Papagaio_Verdadeiro"
+# BY RUANZINH
 
 # Set ur libname here
 LIB_NAME="libTool.so"
@@ -32,66 +32,87 @@ GREEN='\033[0;32m'
 CYAN='\033[0;36m'
 RED='\033[0;31m'
 NC='\033[0m'
+PURPLE='\033[0;35m'
 
 # Payload Base64
-PAYLOAD="$B64_DATA"
+PAYLOAD="__PAYLOAD__"
 
 clear
-echo -e "\${CYAN}=== LIB Injector ===\${NC}"
+echo -e "${CYAN}=== Lib Injector via PTRACE ===${NC}"
 
-if [ "\$(id -u)" -ne 0 ]; then
-    echo -e "\${RED}[!] ROOT Access Required.\${NC}"
+echo -e "${GREEN}[TELEGRAM]: @Papagaio_Verdadeiro${NC}"
+
+if [ "$(id -u)" -ne 0 ]; then
+    echo -e "${RED}[!] ROOT Access Required.${NC}"
     exit 1
 fi
 
-echo -e "\${CYAN}[*] Extracting injector...\${NC}"
-echo "\$PAYLOAD" | base64 -d > "\$TEMP_BIN"
-chmod 777 "\$TEMP_BIN"
+trap 'setenforce 1; rm -f "$TEMP_BIN"' EXIT
 
-if [ ! -s "\$TEMP_BIN" ]; then
-    echo -e "\${RED}[!] Critical error while extracting.\${NC}"
-    rm "\$TEMP_BIN"
+setenforce 0
+echo -e "${PURPLE}[*] SELinux set to Permissive.${NC}"
+
+echo -e "${CYAN}[*] Extracting injector...${NC}"
+echo "$PAYLOAD" | base64 -d > "$TEMP_BIN"
+chmod 777 "$TEMP_BIN"
+
+if [ ! -s "$TEMP_BIN" ]; then
+    echo -e "${RED}[!] Critical error in extraction.${NC}"
+    setenforce 1
+    echo -e "${PURPLE}[*] SELinux set back to Enforcing.${NC}"
+    rm "$TEMP_BIN"
     exit 1
 fi
 
-echo -e "\${GREEN}[?] Game Package: (ex: com.shootergamesonline.blockstrike):\${NC}"
+echo "CURRENT PATH: ${ORIGIN_PATH}\nTARGET LIB: ${LIB_NAME}"
+
+echo -e "${GREEN}[?] Game Package (ex: com.shootergamesonline.blockstrike):${NC}"
 echo -n "> "
 read PACKAGE_NAME
 
-if [ -z "\$PACKAGE_NAME" ]; then
-    echo -e "\${RED}[!] Canceled.\${NC}"
-    rm "\$TEMP_BIN"
+if [ -z "$PACKAGE_NAME" ]; then
+    echo -e "${RED}[!] Canceled.${NC}"
+    setenforce 1
+    echo -e "${PURPLE}[*] SELinux set back to Enforcing.${NC}"
+    rm "$TEMP_BIN"
     exit 1
 fi
 
-echo -e "\${CYAN}[*] Preparing...\${NC}"
-setenforce 0
-if [ -f "\$ORIGIN_PATH" ]; then
-    cp "\$ORIGIN_PATH" "\$TEMP_LIB_PATH"
-    chmod 777 "\$TEMP_LIB_PATH"
+echo -e "${CYAN}[*] Preparing...${NC}"
+
+if [ -f "$ORIGIN_PATH" ]; then
+    cp "$ORIGIN_PATH" "$TEMP_LIB_PATH"
+    chmod 777 "$TEMP_LIB_PATH"
 else
-    echo -e "\${RED}[!] Lib not found.\${NC}"
-    rm "\$TEMP_BIN"
+    echo -e "${RED}[!] Lib not found. ${NC}"
+    setenforce 1
+    echo -e "${PURPLE}[*] SELinux set back to Enforcing.${NC}"
+    rm "$TEMP_BIN"
     exit 1
 fi
 
-echo -e "\${CYAN}[*] Starting Game...\${NC}"
-monkey -p "\$PACKAGE_NAME" -c android.intent.category.LAUNCHER 1 > /dev/null 2>&1
+echo -e "${CYAN}[*] Starting game...${NC}"
+monkey -p "$PACKAGE_NAME" -c android.intent.category.LAUNCHER 1 > /dev/null 2>&1
 sleep 3
 
-PID=\$(pidof -s "\$PACKAGE_NAME")
-if [ -z "\$PID" ]; then
-    echo -e "\${RED}[!] Game closed.\${NC}"
-    rm "\$TEMP_BIN"
+PID=$(pidof -s "$PACKAGE_NAME")
+if [ -z "$PID" ]; then
+    echo -e "${RED}[!] Game closed.${NC}"
+    setenforce 1
+    echo -e "${PURPLE}[*] SELinux set back to Enforcing.${NC}"
+    rm "$TEMP_BIN"
     exit 1
 fi
 
-echo -e "\${GREEN}[*] Injecting...\${NC}"
-"\$TEMP_BIN" "\$PID" "\$TEMP_LIB_PATH"
+echo -e "${GREEN}[*] Injecting...${NC}"
+"$TEMP_BIN" "$PID" "$TEMP_LIB_PATH"
 
-rm "\$TEMP_BIN"
-echo -e "\${GREEN}[DONE] Success.\${NC}"
+rm "$TEMP_BIN"
+echo -e "${GREEN}[DONE] Success.${NC}"
+setenforce 1
+echo -e "${PURPLE}[*] SELinux set back to Enforcing.${NC}"
 EOF
+sed -i "s|__PAYLOAD__|$B64_DATA|" "$OUTPUT_SCRIPT"
 
 chmod +x "$OUTPUT_SCRIPT"
 echo "[OK] File '$OUTPUT_SCRIPT' created successfully!"
